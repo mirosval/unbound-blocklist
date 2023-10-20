4f5875b83b485970336910ec6b128de3406169e5
Miroslav Zoricak <miroslav.zoricak@gmail.com>
2023-10-20 13:44:13 +0200

    move nixosModule out of eachSystem

--- flake.nix
+++ flake.nix
@@ -5,50 +5,52 @@
     flake-utils.url = github:numtide/flake-utils;
   };
   outputs = inputs@{ self, nixpkgs, flake-utils, ... }:
-    flake-utils.lib.eachSystem flake-utils.lib.allSystems (system:
-      let
-        pkgs = import nixpkgs { inherit system; };
-        confName = "blacklist.conf";
-      in
-      {
-        packages.default = pkgs.stdenv.mkDerivation
-          {
-            name = "stevenblack-unbound";
-            version = "2023-10-20";
-            src = pkgs.fetchFromGitHub
-              {
-                owner = "StevenBlack";
-                repo = "host";
-                rev = "master";
-                hash = "sha256-fPMGNj1dXrbxJDxiC8U41NLz1vL5m3Ayw8uC1HJm4sU=";
-              };
+    flake-utils.lib.eachSystem flake-utils.lib.allSystems
+      (system:
+        let
+          pkgs = import nixpkgs { inherit system; };
+          confName = "blacklist.conf";
+        in
+        {
+          packages.default = pkgs.stdenv.mkDerivation
+            {
+              name = "stevenblack-unbound";
+              version = "2023-10-20";
+              src = pkgs.fetchFromGitHub
+                {
+                  owner = "StevenBlack";
+                  repo = "host";
+                  rev = "master";
+                  hash = "sha256-fPMGNj1dXrbxJDxiC8U41NLz1vL5m3Ayw8uC1HJm4sU=";
+                };
 
-            sourceRoot = ".";
+              sourceRoot = ".";
 
-            installPhase = ''
-              grep '^0\.0\.0\.0' source/hosts | awk '{print "local-zone: \""$2"\" always_null"}' > blacklist.conf
-              mkdir -p $out
-              cp blacklist.conf $out/${confName}
-            '';
+              installPhase = ''
+                grep '^0\.0\.0\.0' source/hosts | awk '{print "local-zone: \""$2"\" always_null"}' > blacklist.conf
+                mkdir -p $out
+                cp blacklist.conf $out/${confName}
+              '';
+            };
+        }
+      ) //
+    {
+      nixosModules.default = { config, lib, pkgs, ... }:
+        with lib;
+        let
+          pkg = self.packages.${pkgs.system}.default;
+          cfg = config.services.unbound.blacklist;
+        in
+        {
+          options.services.unbound.blacklist = {
+            enable = mkEnableOption "Enables DNS blacklist generated from StevenBlack hosts file";
           };
 
-        nixosModules.default = { config, lib, pkgs, ... }:
-          with lib;
-          let
-            pkg = self.packages.${pkgs.system}.default;
-            cfg = config.services.unbound.blacklist;
-          in
-          {
-            options.services.unbound.blacklist = {
-              enable = mkEnableOption "Enables DNS blacklist generated from StevenBlack hosts file";
-            };
-
-            config = mkIf cfg.enable {
-              services.unbound.settings = {
-                include = "${pkg}/${confName}";
-              };
+          config = mkIf cfg.enable {
+            services.unbound.settings = {
+              include = "${pkg}/${confName}";
             };
           };
-      }
-    );
+        };
+    };
 }
